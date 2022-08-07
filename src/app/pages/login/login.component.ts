aa094d4c42efd078b4a317501e751c2be6d2d6bb
megafatayat <P041121007@bussan.co.id>
2022-08-07 12:05:43 +0700

    login done

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AlertMessage } from 'src/app/shared/models/alert-message-interface';
+import { LoginAdmin } from 'src/app/shared/models/login-admin';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,83 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loading: boolean = false;
+  loginAdmin!: LoginAdmin;
+  message?: AlertMessage;
+  
+  loginForm: FormGroup = new FormGroup({
+    username: new FormControl(null, [Validators.required, Validators.minLength(3)]),
+    password: new FormControl(null, [Validators.required, Validators.minLength(8)]),
+  })
+  
+  constructor(
+    private readonly router: Router
+  ) { }
 
   ngOnInit(): void {
   }
 
+  renderPassErrMessage():string {
+    const errorObj = this.loginForm.controls?.email?.errors
+    if(errorObj?.required){
+      return 'Email harus diisi'
+    }
+    return 'Email tidak valid'
+  }
+
+  isFieldValid(fieldName: string, parent?: AbstractControl): {[key: string]: boolean} {
+    let control: AbstractControl = this.loginForm.get(fieldName) as AbstractControl;
+
+    const classes = {
+      'is-invalid': false,
+      'is-valid': false
+    }
+
+    if (parent) {
+      control = parent;
+    }
+    if (control && control.touched && control.invalid) {
+      classes['is-invalid'] = true;
+    } else if (control && control.valid) {
+      classes['is-valid'] = true;
+    } 
+    
+    return classes;
+  }
+
+  onSubmit(): void {
+    this.loading = true;
+    const loginAdmin: LoginAdmin = this.loginForm.value;
+    if (this.loginForm.value.username === "alFaqir") {
+      if (this.loginForm.value.password === "H@sbiRobbi") {
+        sessionStorage.setItem('username', loginAdmin.username);
+
+        this.message = {
+          status: 'success',
+          text: `Selamat, ${loginAdmin.username} berhasil login!`
+        }
+      } else {
+        this.message = {
+          status: 'warning',
+          text: `Maaf, ${loginAdmin.username}. Password salah.`
+        }
+      }
+    } else {
+      this.message = {
+        status: 'danger',
+        text: `Maaf, ${loginAdmin.username} tidak terdaftar sebagai admin`
+      }
+    }
+
+    setTimeout(() => {
+      this.message = undefined;
+    }, 10000);
+
+    this.onReset();
+    this.router.navigateByUrl('/login');
+  }
+
+  onReset(): void {
+    this.loginForm.reset();
+  }
 }
---
+++ src/app/shared/models/login-admin.ts
@@ -0,0 +1,4 @@
+export interface LoginAdmin {
+    username: string;
+    password: string;
+  }
