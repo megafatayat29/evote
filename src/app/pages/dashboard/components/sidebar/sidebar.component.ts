45c650426110236c979ce7b402015835da5efd3f
megafatayat <P041121007@bussan.co.id>
2022-08-27 08:53:28 +0700

    dashboard admin page: send email verif

--- src/app/pages/dashboard/components/sidebar/sidebar.component.ts
+++ src/app/pages/dashboard/components/sidebar/sidebar.component.ts
@@ -1,13 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss']
 })
-export class SidebarComponent implements OnInit {
+export class SidebarComponent implements OnInit, OnChanges {
 
   constructor() { }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes);
+  }
 
   ngOnInit(): void {
   }
